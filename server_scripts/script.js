53eaa853fa5ce50be875cb17b912d1eb3c02c2cb
Gcat101 <gcatmail2@gmail.com>
2023-07-08 17:16:07 +0300

    add the script

---
+++ server_scripts/script.js
@@ -0,0 +1,17 @@
+let targetLength = global.dayLength; // in minutes
+const pauseDivision = 20;
+const celestialRepositionTimer = 20 * MINUTE / pauseDivision;
+const skyPauseTimer = ((targetLength - 20) * MINUTE) / pauseDivision;
+ServerEvents.loaded((event) => {
+    if (targetLength==null) return;
+    const gameRules = event.server.getOverworld().getGameRules();
+  event.server.schedule(celestialRepositionTimer, (callback) => {
+    if (gameRules.get("doDaylightCycle").get()) {
+      gameRules.set("doDaylightCycle", false);
+      callback.reschedule(skyPauseTimer);
+    } else {
+      gameRules.set("doDaylightCycle", true);
+      callback.reschedule(celestialRepositionTimer);
+    }
+  });
+});
